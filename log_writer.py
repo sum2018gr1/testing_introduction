3be9649ae4e304d8858ff69afd9fd677dc68e6f4
Bartlomiej  Michalski <4michalski@stud105>
2018-03-21 17:36:50 +0100

    3

--- log_writer.py
+++ log_writer.py
@@ -37,7 +37,7 @@ class LogWriter(object):
 		# insert_data_in_text("AAAA list BBBB", [1,2,3]) = "AAAA list ([1, 2, 3]) BBBB"
 		l_index = text.find("list")
 		r_index = text.rfind("list")		
-		if	not l_index == -1 and l_index == r_index:
+		if not l_index == -1 and l_index == r_index:
 			result = "{}({}) {}".format(text[:l_index], str(data),text[l_index:])
 			return result		
 		else:
